aeeddd3074bde513f94fec2bf7fb8f06f61fedee
Ruth Mariya S <Ruth.Mariya.S@ibm.com>
2023-06-05 14:26:06 +0530

    modified sample.py

--- sample.py
+++ sample.py
@@ -3,5 +3,4 @@ def add_num(a,b):
     return sum; 
 num1=int(input("input the number one: "))
 num2=int(input("input the number one :"))
-
-print("The sum is",add_num(num1,num2))
+add_num(num1,num2))
